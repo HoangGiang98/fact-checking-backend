9373cb1158db24bdbd254791a381b92e6ffc4311
gvu <gvu@inovex.de>
2022-11-27 14:51:36 +0100

    initial implementation of integrating api, crawling and preprocessing

--- fact_checking_service/settings.py
+++ fact_checking_service/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "applications.factchecker"
+    "applications.factchecker",
+    "applications.apisearch"
 ]
 
 MIDDLEWARE = [
