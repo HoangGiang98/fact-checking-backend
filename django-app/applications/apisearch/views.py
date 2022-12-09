a4e5becc0de24d9bc602c818948026a578a4ccda
gvu <gvu@inovex.de>
2022-12-09 05:35:57 +0100

    add urls and views

--- django-app/applications/apisearch/views.py
+++ django-app/applications/apisearch/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+
+from .serializers import ApiSearchRequestSerializer
+from .models import ApiSearchRequest
+
+
+def health(request):
+    return HttpResponse("ApiSearch is running")
 
 # Create your views here.
+@api_view(["GET"])
+def history(request):
+  latest = ApiSearchRequest.objects.all().order_by("-datetime")[:20]
+
+  serializer = ApiSearchRequestSerializer(latest, many=True)
+
+  return HttpResponse(serializer.data, content_type="application/json")
+
+
+@api_view(["POST"])
+def verify(request):
+  serializer = ApiSearchRequestSerializer(data=request.data)
+  serializer.is_valid(raise_exception=True)
+  serializer.save()
+
+  dto = serializer.Meta.model
+  print(dto.claim)
+  print(dto.answer)
+
+  return HttpResponse("Answer")
